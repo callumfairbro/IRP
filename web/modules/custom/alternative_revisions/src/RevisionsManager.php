2d98e9a9e2e38d739131a84570d469d6d66870a1
callumfairbro <callum.fairbrother11@gmail.com>
2023-10-31 22:20:11 +0000

    Adding additional schema, and install and uninstall functions

--- web/modules/custom/alternative_revisions/src/RevisionsManager.php
+++ web/modules/custom/alternative_revisions/src/RevisionsManager.php
@@ -39,16 +39,20 @@ class RevisionsManager {
                 $spec = $this->getTextLongSpec($field_name);
                 break;
             case "text_with_summary":
+                $spec = $this->getTextSummarySpec($field_name);
                 break;
             case "entity_reference":
+                $spec = $this->getEntityReferenceSpec($field_name);
                 break;
             case "entity_reference_revisions":
+                // Add support for paragraphs
                 break;
             case "image":
+                $spec = $this->getBasicImageSpec($field_name);
                 break;
             case "datetime":
+                $spec = $this->getDateTimeSpec($field_name);
                 break;
-            default:
         }
         if ($spec) {
             $this->schema->createTable($table_name, $spec);
@@ -66,24 +70,224 @@ class RevisionsManager {
               'entity_id' => [
                 'type' => 'int',
                 'not null' => TRUE,
-                'default' => NULL,
                 'unsigned' => TRUE,
               ],
-              $field_name . '_value' => [
-                'type' => 'longtext',
+              'bundle' => [
+                'type' => 'varchar',
+                'length' => 128,
                 'not null' => TRUE,
-                'default' => NULL,
+              ],
+              'delta' => [
+                'type' => 'int',
+                'not null' => TRUE,
+              ],
+              $field_name . '_value' => [
+                'type' => 'text',
+                'size' => 'big',
+                'not null' => TRUE,
               ],
               $field_name . '_format' => [
                 'type' => 'varchar',
-                'not null' => FALSE,
                 'length' => 255,
+                'not null' => FALSE,
                 'default' => NULL,
               ],
               'revision_date' => [
                 'type' => 'int',
                 'not null' => TRUE,
-                'default' => time(),
+                'default' => 0,
+              ],
+            ],
+            'primary key' => ['id'],
+        ];
+        return $spec;
+    }
+
+    private function getTextSummarySpec(String $field_name) {
+        $spec = [
+            'description' => 'Text with summary database table.',
+            'fields' => [
+              'id' => [
+                'type' => 'serial',
+                'not null' => TRUE,
+              ],
+              'entity_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              'bundle' => [
+                'type' => 'varchar',
+                'length' => 128,
+                'not null' => TRUE,
+              ],
+              'delta' => [
+                'type' => 'int',
+                'not null' => TRUE,
+              ],
+              $field_name . '_value' => [
+                'type' => 'text',
+                'size' => 'big',
+                'not null' => TRUE,
+              ],
+              $field_name . '_summary' => [
+                'type' => 'text',
+                'size' => 'big',
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              $field_name . '_format' => [
+                'type' => 'varchar',
+                'length' => 255,
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              'revision_date' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'default' => 0,
+              ],
+            ],
+            'primary key' => ['id'],
+        ];
+        return $spec;
+    }
+
+    private function getBasicImageSpec(String $field_name) {
+        $spec = [
+            'description' => 'Image database table.',
+            'fields' => [
+              'id' => [
+                'type' => 'serial',
+                'not null' => TRUE,
+              ],
+              'entity_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              'bundle' => [
+                'type' => 'varchar',
+                'length' => 128,
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              'delta' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              $field_name . '_target_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              $field_name . '_alt' => [
+                'type' => 'varchar',
+                'length' => 512,
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              $field_name . '_title' => [
+                'type' => 'varchar',
+                'length' => 1024,
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              $field_name . '_width' => [
+                'type' => 'int',
+                'not null' => FALSE,
+                'default' => NULL,
+                'unsigned' => TRUE,
+              ],
+              $field_name . '_height' => [
+                'type' => 'int',
+                'not null' => FALSE,
+                'default' => NULL,
+                'unsigned' => TRUE,
+              ],
+              'revision_date' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'default' => 0,
+              ],
+            ],
+            'primary key' => ['id'],
+        ];
+        return $spec;
+    }
+
+    private function getEntityReferenceSpec(String $field_name) {
+        $spec = [
+            'description' => 'Entity reference database table.',
+            'fields' => [
+              'id' => [
+                'type' => 'serial',
+                'not null' => TRUE,
+              ],
+              'entity_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              'bundle' => [
+                'type' => 'varchar',
+                'length' => 128,
+                'not null' => FALSE,
+                'default' => NULL,
+              ],
+              'delta' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              $field_name . '_target_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              'revision_date' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'default' => 0,
+              ],
+            ],
+            'primary key' => ['id'],
+        ];
+        return $spec;
+    }
+
+    private function getDateTimeSpec(String $field_name) {
+        $spec = [
+            'description' => 'Date time database table.',
+            'fields' => [
+              'id' => [
+                'type' => 'serial',
+                'not null' => TRUE,
+              ],
+              'entity_id' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'unsigned' => TRUE,
+              ],
+              'bundle' => [
+                'type' => 'varchar',
+                'length' => 128,
+                'not null' => TRUE,
+              ],
+              'delta' => [
+                'type' => 'int',
+                'not null' => TRUE,
+              ],
+              $field_name . '_value' => [
+                'type' => 'varchar',
+                'length' => 20,
+                'not null' => TRUE,
+              ],
+              'revision_date' => [
+                'type' => 'int',
+                'not null' => TRUE,
+                'default' => 0,
               ],
             ],
             'primary key' => ['id'],
